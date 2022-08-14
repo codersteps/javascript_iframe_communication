654b5c17aed7dbc2d5d587490a95fd778e4b27a7
Abdessamad El Hamdany <abdessamadelhamdany@gmail.com>
2022-08-14 14:10:14 +0100

    The iframe page

--- src/client/iframe.ts
+++ src/client/iframe.ts
@@ -1,1 +1,17 @@
-console.log('Hello from iframe')
+window.onload = () => {
+  const registrationForm = document.querySelector('form[name=registration]')
+  if (!registrationForm) {
+    return
+  }
+
+  registrationForm.addEventListener('submit', (e: Event) => {
+    e.preventDefault()
+
+    const formElement = e.target as HTMLFormElement
+    const formData = new FormData(formElement)
+
+    const userData = Object.fromEntries(formData.entries())
+
+    console.log('userData', userData)
+  })
+}
